4a4b3e794380a065b74283b027aab89286aaaf24
NahuFalconaro <nfalconaro5@gmail.com>
2021-10-04 19:58:36 -0300

    Cambio en section servicios, agregado de cartas describiendo los servicios

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// window.addEventListener('scroll', ()=>{
+//             const header = document.querySelector('header');
+//             header.classList.toggle("sticky", window.scrollY > 0);
+
+//             hello()
+//         });
+
+// function hello(){
+//     console.log("hola")
+// }
+
+
